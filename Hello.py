5663f810b3e3d1150b2e565a17415303038e203b
Rohit <rohit.d@innovify.in>
2017-09-28 13:09:30 +0530

    python hello world example
    python hello world example

---
+++ Hello.py
@@ -0,0 +1,25 @@
+
+a = 2
+b = 3
+c = a**b
+d = 5//a
+b +=  7
+
+
+def sum():
+    #z = a + b
+    return complex(a,b)
+
+sum()
+print "value.txt of a" ,a,b,c,d,b
+z = sum()
+print "z",z
+
+# print "Whats your Good Name"
+# name = raw_input()
+# print name
+
+# name = raw_input("Hi Rohit, Whats your profession")
+# print name, name
+
+print "----" * 20
